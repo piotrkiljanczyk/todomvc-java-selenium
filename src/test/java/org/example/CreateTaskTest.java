989a35e50c5e5da5d4da91b5395a9052a89235ea
PKiljanczyk1 <iPiotr87#>
2022-10-04 10:41:50 +0200

    Refactor 2

--- src/test/java/org/example/CreateTaskTest.java
+++ src/test/java/org/example/CreateTaskTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -12,14 +14,41 @@ public class CreateTaskTest {
 
     WebDriver browser = new FirefoxDriver();
 
+    @BeforeEach
+    public void setup() {
+        userOpensTodoMVC();
+    }
+
+    @AfterEach
+    public void cleanup() {
+        browser.close();
+    }
+
     @Test
-    public void userCanCreateATodo() throws InterruptedException {
+    public void userCanCreateATodo() {
 
         var todoName = "To jest moje zadanie";
-        userOpensTodoMVC();
         userCreatesANewTodo(todoName);
         userChecksIfTodoWasCreated(todoName);
-        browser.close();
+    }
+
+    @Test
+    public void userCanCompleteTodo() {
+        var todoName = "To jest moje samodzielne zadanie";
+        userCreatesANewTodo(todoName);
+        userCompleteTodoTask();
+        userChecksItTodoIsMarkedAsCompleted();
+    }
+
+    private void userChecksItTodoIsMarkedAsCompleted() {
+        var todoElement = browser.findElement(By.cssSelector(".todo-list > li"));
+        String todoClasses = todoElement.getAttribute("class");
+        MatcherAssert.assertThat("Todo should be marked as completed", todoClasses, Matchers.equalTo("completed"));
+    }
+
+    private void userCompleteTodoTask() {
+        var todosCheck = browser.findElement(By.cssSelector(".toggle"));
+        todosCheck.click();
     }
 
     private void userChecksIfTodoWasCreated(String todoName) {
